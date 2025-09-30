c99d64e47992ef0569f116a3d06cf8c9db2fcf4d
amir <amirtaki623@gmail.com>
2025-09-30 21:41:28 +0330

    part 21

--- src/main.js
+++ src/main.js
@@ -4,3 +4,6 @@ const filterImg = document.querySelectorAll(".gallery .image")
 const previewBox = document.querySelector(".preview-box")
 const categoryName = previewBox.querySelector(".title p")
 const previewImg = previewBox.querySelector("img")
+const closeIcon = previewBox.querySelector(".icon")
+const shadow = previewBox.querySelector(".shadow")
+
