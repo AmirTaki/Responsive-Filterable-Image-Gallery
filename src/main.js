4a970a9501b444ede71d6f4f81c63f0ba6af0ebf
amir <amirtaki623@gmail.com>
2025-09-30 21:40:06 +0330

    part 19

--- src/main.js
+++ src/main.js
@@ -1,2 +1,5 @@
 const filterItem = document.querySelector(".items")
 const filterImg = document.querySelectorAll(".gallery .image")
+
+const previewBox = document.querySelector(".preview-box")
+const categoryName = documpreviewBoxent.querySelector(".title p")
