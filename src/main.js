d48972484af8b16566d42c6f416dd8e957905b06
amir <amirtaki623@gmail.com>
2025-09-30 22:05:43 +0330

    part 33

--- src/main.js
+++ src/main.js
@@ -29,12 +29,19 @@ const preview  = (element) => {
 
 window.onload = () => {
     filterItem.onClick = (selectedItem) => {
+       
         if(selectedItem.target.classList.contains('item')){
             filterItem.querySelector('.active').classList.remove('active')
             selectedItem.target.classList.add('active')   
+         
             let filterName = selectedItem.target.getAttribute('data-name')
+         
             filterImg.forEach((image) => {
-                let filterimages = image.getAttribute('data-name')
+                let filterImages = image.getAttribute('data-name')
+
+                if((filterImages == filterName) || (filterName == 'all')){
+                    
+                }
             })
         }
     }
